e197245db6a8629cec6de29595f83c045345dbc0
n3n7i <25413904+n3n7i@users.noreply.github.com>
2026-07-02 11:49:23 +0800

    Update accessPoint.js

--- src/accessPoint.js
+++ src/accessPoint.js
@@ -1,1 +1,4 @@
-var xloc = "https://side-young-mountain.solana-mainnet.discover.quiknode.pro/cf23f5a294367ce0cf1b89a1a34d9c00039e069a/"
+//var xloc = "https://side-young-mountain.solana-mainnet.discover.quiknode.pro/cf23f5a294367ce0cf1b89a1a34d9c00039e069a/"
+
+var xloc = "https://solana-rpc.publicnode.com";
+
